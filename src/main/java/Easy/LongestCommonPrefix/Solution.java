08df1228944932add51cfe6b332865a544d57923
Axel Joshue Fiestas Santisteban <89173967+Axel-Fiestas@users.noreply.github.com>
2025-01-21 18:33:47 -0500

    feat: Thinking in a solution...

--- src/main/java/Easy/LongestCommonPrefix/Solution.java
+++ src/main/java/Easy/LongestCommonPrefix/Solution.java
@@ -4,6 +4,28 @@ public class Solution {
 
     public String longestCommonPrefix(String[] strs){
 
+        if(strs == null) return "";
+
+        String firstLetter="";
+
+        for(String word: strs){
+
+            //Agarramos la primera letra de la palabra
+            if(firstLetter==""){
+                firstLetter=""+word.charAt(0);
+            }else{
+
+                //Si ya pasó la primera palabra, osea estamos en la segunda
+                String firstLetterFromNextWord= ""+word.charAt(0);
+                //Comparamos
+                if(firstLetter== firstLetterFromNextWord){
+
+                }
+            }
+
+
+
+        }
         return "";
     }
 }
