20d64288f1ad8ad13d0eca7221a40dff5f87806c
Axel Joshue Fiestas Santisteban <89173967+Axel-Fiestas@users.noreply.github.com>
2025-01-23 21:59:49 -0500

    feat: Pensando en una solución otra vez...

--- src/main/java/Easy/LongestCommonPrefix/Solution.java
+++ src/main/java/Easy/LongestCommonPrefix/Solution.java
@@ -4,28 +4,56 @@ public class Solution {
 
     public String longestCommonPrefix(String[] strs){
 
-        if(strs == null) return "";
 
-        String firstLetter="";
+        //flower,flow,flight
 
-        for(String word: strs){
+        String prefix = "";
+        //Un auxiliar para ayudarme
+        String aux = "";
+        int iteratorLetras=0;
 
-            //Agarramos la primera letra de la palabra
-            if(firstLetter==""){
-                firstLetter=""+word.charAt(0);
-            }else{
+        //bucle para cada palabra
+        for(int i=0;i<strs.length;i++) {
 
-                //Si ya pasó la primera palabra, osea estamos en la segunda
-                String firstLetterFromNextWord= ""+word.charAt(0);
-                //Comparamos
-                if(firstLetter== firstLetterFromNextWord){
+            String word = strs[i];
+            //flower
+
+            //Agarro la priemera letra
+            String firstLetter;
+
+
+            //Agarro la primera letra
+            firstLetter=""+word.charAt(iteratorLetras);
+
+            //Si el auxiliar no tiene nada (osea estoy en el primer bucle le proporciono el valor del firstLetter)
+            if(aux.isEmpty()) aux=firstLetter;
+            //Si tiene algo entonces lo comparo el auxiliar con la firstLetter de la nueva palabra
+            else{
+
+                //Si el auxiliar con el firstLetter
+                if(aux==firstLetter){
+                    aux=firstLetter;
+
+                    //Si estamos en la última palabra
+                    if(i==strs.length-1){
+                        prefix+=aux;
+
+                        iteratorLetras++;
+                        //Aquí deberíamos hacer que el bucle for vuelva al inicio pero ahora con el nuevo iterator para seguir comparando
+
+                    }
 
                 }
+                //Si son distintos rompo el bucle
+                else{
+                    break;
+                }
+
             }
 
 
-
         }
-        return "";
+
+        return prefix;
     }
 }
