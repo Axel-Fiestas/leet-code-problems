af44a8345e92ce2c45245f6eb4a7f45d24bae824
Axel Joshue Fiestas Santisteban <89173967+Axel-Fiestas@users.noreply.github.com>
2025-02-04 16:57:18 -0500

    feat: Pensando que hacer...

--- src/main/java/Easy/MergeTwoSortedLists/Solution.java
+++ src/main/java/Easy/MergeTwoSortedLists/Solution.java
@@ -13,6 +13,12 @@ package Easy.MergeTwoSortedLists;
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
+        // La lista ordenada final debe de tener la misma capacidad de la list1.val + list2. val , si la lista 1 tiene 3 nodos y la lista 2 4 nodos
+        // eso nos daría que la lista final debe tener un total de capacidad de 7 nodos
+        ListNode finalList = new ListNode(list1.val + list2.val);
+
+
+
         return null;
     }
 }
